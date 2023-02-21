117f10de271ba612d62b87fa65b738bec4a17947
RafalW <rafal.wielicki@gmail.com>
2023-02-21 09:24:07 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -12,18 +12,14 @@
               @menuClicked="isNavigationBarOpen = !isNavigationBarOpen"
               @applyClicked="$root.$emit('settleTasks')"
               :isApplyEnabled="isTaskListSelection"/>
-              <h5 class="px-2 py-1 yellow">HOOKS</h5>
-              <h5 class="px-2 py-1 yellow">Dodaj trigger do Tasks usuwajacy rekord z Clients jesli ostatni</h5>
-              <h5 class="px-2 py-1 yellow">Dodaj trigger do Tasks usuwajacy rekord z Projects jesli ostatni</h5>
-              <h5 class="px-2 py-1 yellow">Pole wersja opcjonalne</h5>
-              <h5 class="px-2 py-1 yellow">TaskListView usun Ilosc godzin dla DataSoft</h5>
+              <!-- <h5 class="px-2 py-1 yellow">TaskListView usun Ilosc godzin dla DataSoft</h5>
               <h5 class="px-2 py-1 yellow">Login link rejestracja</h5>
               <h5 class="px-2 py-1 yellow">Auto message</h5>
               <h5 class="px-2 py-1 yellow">Widok portret tylko szukaj na wierzchu, reszta po rozwinieciu</h5>
               <h5 class="px-2 py-1 yellow">Widok portret do poprawy na telefonie</h5>
               <h5 class="px-2 py-1 yellow">Widok portret Klienci normalna lista</h5>
               <h5 class="px-2 py-1 yellow">Przypisz projekty do klienta</h5>
-              <h5 class="px-2 py-1 yellow">Wczytuj wersje po wpisaniu projektu</h5>
+              <h5 class="px-2 py-1 yellow">Wczytuj wersje po wpisaniu projektu</h5> -->
             <router-view
               :key="$route.path"
               @isProcessing="isProcessing = $event"
