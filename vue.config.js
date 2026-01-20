14102ec3326f1cd5575e4b9411afbe356e880a17
Rafal Wielicki <rafal.wielicki@gmail.com>
2026-01-20 21:44:25 +0100

    Trying to fix webbrowser old version caching issue

--- vue.config.js
+++ vue.config.js
@@ -7,10 +7,22 @@ module.exports = defineConfig({
   publicPath: '/settlement/',
   pluginOptions: {
     i18n: {
-      locale: "en",
-      fallbackLocale: "en",
-      localeDir: "locales",
-      enableInSFC: false
-    }
-  }
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false,
+    },
+  },
+  configureWebpack: {
+    output: {
+      hashFunction: 'sha256',
+      hashDigestLength: 8,
+    },
+  },
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].version = new Date().getTime(); // Add a timestamp to force cache busting
+      return args;
+    });
+  },
 });
